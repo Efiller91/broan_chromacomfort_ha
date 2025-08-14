dc1e03e76125c5f5829a728df101e99152126f81
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 15:57:08 -0700

    Update fan.py

--- custom_components/broan_chromacomfort/fan.py
+++ custom_components/broan_chromacomfort/fan.py
@@ -3,19 +3,25 @@ from __future__ import annotations
 
 import logging
 from homeassistant.components.fan import FanEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
+from .bluetooth import connect_to_device
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([BroanFan()])
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    """Set up Broan ChromaComfort fan."""
+    async_add_entities([BroanFan(entry.data["device_mac"])])
 
 
 class BroanFan(FanEntity):
     """Representation of the ChromaComfort fan."""
 
-    def __init__(self):
+    def __init__(self, mac: str):
         self._is_on = False
+        self._mac = mac
 
     @property
     def name(self):
@@ -27,10 +33,14 @@ class BroanFan(FanEntity):
 
     async def async_turn_on(self, **kwargs):
         _LOGGER.info("Turning on ChromaComfort fan")
-        self._is_on = True
-        self.async_write_ha_state()
+        success = await connect_to_device(self._mac)
+        if success:
+            self._is_on = True
+            self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
         _LOGGER.info("Turning off ChromaComfort fan")
-        self._is_on = False
-        self.async_write_ha_state()
+        success = await connect_to_device(self._mac)
+        if success:
+            self._is_on = False
+            self.async_write_ha_state()
