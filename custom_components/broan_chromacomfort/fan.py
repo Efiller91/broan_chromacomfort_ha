0d25d6bcd058a89c88a52d5ef255c5b6d3f48b41
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 18:38:17 -0700

    Update fan.py

--- custom_components/broan_chromacomfort/fan.py
+++ custom_components/broan_chromacomfort/fan.py
@@ -1,49 +1,35 @@
 """Fan platform for ChromaComfort."""
 
-from homeassistant.components.fan import FanEntity, SUPPORT_SET_SPEED
-from homeassistant.helpers.entity import Entity
-
+from homeassistant.components.fan import FanEntity, SUPPORT_ON_OFF
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
-
-SPEEDS = ["low", "medium", "high"]
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up fan from a config entry."""
-    ble_client = hass.data[DOMAIN]["ble_client"]
-    async_add_entities([ChromaComfortFan(ble_client)], True)
 
 class ChromaComfortFan(FanEntity):
     """Representation of the ChromaComfort fan."""
 
-    def __init__(self, ble_client):
+    def __init__(self, hass, ble_client):
         self._ble = ble_client
         self._is_on = False
-        self._speed = "medium"
+        self._name = "ChromaComfort Fan"
 
     @property
     def name(self):
-        return "ChromaComfort Fan"
+        return self._name
 
     @property
     def is_on(self):
         return self._is_on
 
     @property
-    def speed(self):
-        return self._speed
+    def supported_features(self):
+        return SUPPORT_ON_OFF
 
-    @property
-    def speed_list(self):
-        return SPEEDS
-
-    async def async_turn_on(self, speed=None, **kwargs):
+    async def async_turn_on(self, **kwargs):
+        await self._ble.turn_fan(True)
         self._is_on = True
-        if speed:
-            self._speed = speed
-        await self._ble.set_fan(self._is_on, self._speed)
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
+        await self._ble.turn_fan(False)
         self._is_on = False
-        await self._ble.set_fan(self._is_on)
         self.async_write_ha_state()
