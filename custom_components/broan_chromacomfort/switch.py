f864c6fa9bcc578aba971b368814847f686b30cd
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 18:33:20 -0700

    Update switch.py

--- custom_components/broan_chromacomfort/switch.py
+++ custom_components/broan_chromacomfort/switch.py
@@ -1,24 +1,34 @@
-"""ChromaComfort switch platform."""
+"""Switch platform for ChromaComfort."""
 
 from homeassistant.components.switch import SwitchEntity
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up switch from a config entry."""
+    ble_client = hass.data[DOMAIN]["ble_client"]
+    async_add_entities([ChromaComfortSwitch(ble_client)], True)
 
 class ChromaComfortSwitch(SwitchEntity):
-    """ChromaComfort Switch entity."""
+    """Representation of the ChromaComfort wall RGB switch."""
 
     def __init__(self, ble_client):
         self._ble = ble_client
         self._is_on = False
 
     @property
+    def name(self):
+        return "ChromaComfort Wall RGB"
+
+    @property
     def is_on(self):
         return self._is_on
 
     async def async_turn_on(self, **kwargs):
-        await self._ble.turn_on_wall_rgb()
         self._is_on = True
+        await self._ble.set_rgb(True)
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        await self._ble.turn_off_wall_rgb()
         self._is_on = False
+        await self._ble.set_rgb(False)
         self.async_write_ha_state()
