80a24cd7ccd0fee5fba81fe3158fd844a9e17cb7
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 19:12:59 -0700

    Update switch.py

--- custom_components/broan_chromacomfort/switch.py
+++ custom_components/broan_chromacomfort/switch.py
@@ -1,19 +1,9 @@
-"""Switch platform for ChromaComfort integration."""
-
 from homeassistant.components.switch import SwitchEntity
-from .ble import ChromaComfortBLE
 
 class ChromaComfortSwitch(SwitchEntity):
-    """Representation of a ChromaComfort Switch."""
-
-    def __init__(self, ble: ChromaComfortBLE, name="Wall RGB"):
-        self._ble = ble
+    def __init__(self, ble_client):
+        self._ble = ble_client
         self._is_on = False
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
 
     @property
     def is_on(self):
@@ -21,16 +11,10 @@ class ChromaComfortSwitch(SwitchEntity):
 
     async def async_turn_on(self, **kwargs):
         self._is_on = True
-        CMD = bytes([58, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0])
-        await self._ble.send_command(CMD)
+        await self._ble.send_command(b'\x3A\x00\x00\x00\x05' + b'\x00'*12)
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
         self._is_on = False
-        CMD = bytes([58, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0])
-        await self._ble.send_command(CMD)
+        await self._ble.send_command(b'\x3A\x00\x00\x00\x06' + b'\x00'*12)
         self.async_write_ha_state()
-
-    async def async_refresh_state(self):
-        """Fetch latest state from BLE device."""
-        await self._ble.refresh_state()
