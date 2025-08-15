a1446ff6338260cf317cad6451547c1357cd612f
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 18:57:09 -0700

    Update light.py

--- custom_components/broan_chromacomfort/light.py
+++ custom_components/broan_chromacomfort/light.py
@@ -1,25 +1,17 @@
-"""Light platform for ChromaComfort."""
+"""Light platform for ChromaComfort integration."""
 
-from homeassistant.components.light import LightEntity
-from .const import DOMAIN
-from .bluetooth import (
-    light_on_cmd,
-    light_off_cmd,
-    activate_fav_color_cmd,
-    deactivate_fav_color_cmd,
-    set_rgb_cmd,
-)
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    ble_client = hass.data[DOMAIN]["ble_client"]
-    async_add_entities([ChromaComfortLight(ble_client)])
+from homeassistant.components.light import LightEntity, SUPPORT_BRIGHTNESS, SUPPORT_COLOR
+from .ble import ChromaComfortBLE
 
 class ChromaComfortLight(LightEntity):
-    def __init__(self, ble_client):
-        self._ble = ble_client
+    """Representation of the ChromaComfort Light."""
+
+    def __init__(self, ble: ChromaComfortBLE):
+        self._ble = ble
         self._is_on = False
-        self._brightness = 255
-        self._rgb = (255, 255, 255)
+        self._brightness = 0
+        self._color = (0, 0, 0)
+        self._attr_supported_features = SUPPORT_BRIGHTNESS | SUPPORT_COLOR
 
     @property
     def is_on(self):
@@ -31,23 +23,24 @@ class ChromaComfortLight(LightEntity):
 
     @property
     def rgb_color(self):
-        return self._rgb
+        return self._color
 
     async def async_turn_on(self, **kwargs):
-        rgb = kwargs.get("rgb_color")
-        brightness = kwargs.get("brightness", 255)
-
-        if rgb:
-            self._rgb = rgb
-            await self._ble.send_command(set_rgb_cmd(*rgb))
-            await self._ble.send_command(activate_fav_color_cmd(brightness))
-        else:
-            await self._ble.send_command(light_on_cmd())
-
         self._is_on = True
+        brightness = kwargs.get("brightness", 255)
+        rgb = kwargs.get("rgb_color", (255, 255, 255))
         self._brightness = brightness
+        self._color = rgb
+        CMD = bytes([58, 0, 0, 0, 0, rgb[0], rgb[1], rgb[2], 5, brightness, 0, 0, 0, 0, 0, 0, 0])
+        await self._ble.send_command(CMD)
+        self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        await self._ble.send_command(deactivate_fav_color_cmd())
-        await self._ble.send_command(light_off_cmd())
         self._is_on = False
+        CMD = bytes([58, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0])
+        await self._ble.send_command(CMD)
+        self.async_write_ha_state()
+
+    async def async_refresh_state(self):
+        """Fetch latest state from BLE device."""
+        await self._ble.refresh_state()
