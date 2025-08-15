d3569ce68a81bcca61c259e565e35c214a45d392
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 19:00:57 -0700

    Update ble.py

--- custom_components/broan_chromacomfort/ble.py
+++ custom_components/broan_chromacomfort/ble.py
@@ -2,6 +2,9 @@
 
 import asyncio
 from bleak import BleakClient
+from .bluetooth import fan_on_bytes, fan_off_bytes, light_on_bytes, light_off_bytes, rgb_on_bytes, rgb_off_bytes
+
+CHARACTERISTIC_UUID = "0000fff3-0000-1000-8000-00805f9b34fb"
 
 class ChromaComfortBLE:
     """BLE wrapper using ESP32 commands."""
@@ -11,20 +14,36 @@ class ChromaComfortBLE:
         self.client = BleakClient(mac)
 
     async def connect(self):
+        """Connect to the BLE device."""
         await self.client.connect()
 
     async def disconnect(self):
+        """Disconnect from the BLE device."""
         if self.client.is_connected:
             await self.client.disconnect()
 
     async def send_command(self, cmd: bytes):
         """Send raw bytes to the device."""
         if self.client.is_connected:
-            CHARACTERISTIC_UUID = "0000fff3-0000-1000-8000-00805f9b34fb"
             await self.client.write_gatt_char(CHARACTERISTIC_UUID, cmd)
 
-    async def refresh_state(self):
-        """Request the current state from the fan/light/RGB."""
-        # Send a "status request" packet (mimicking the ESP32 status update)
-        STATUS_REQUEST_CMD = bytes([58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        await self.send_command(STATUS_REQUEST_CMD)
+    # Fan commands
+    async def fan_on(self):
+        await self.send_command(fan_on_bytes())
+
+    async def fan_off(self):
+        await self.send_command(fan_off_bytes())
+
+    # Light commands
+    async def light_on(self):
+        await self.send_command(light_on_bytes())
+
+    async def light_off(self):
+        await self.send_command(light_off_bytes())
+
+    # RGB commands
+    async def rgb_on(self, r: int, g: int, b: int, brightness: int = 100):
+        await self.send_command(rgb_on_bytes(r, g, b, brightness))
+
+    async def rgb_off(self):
+        await self.send_command(rgb_off_bytes())
