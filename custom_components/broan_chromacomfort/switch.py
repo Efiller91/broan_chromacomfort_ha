0e4adb24f112c2ed496df674e666813051394db8
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 18:57:23 -0700

    Update switch.py

--- custom_components/broan_chromacomfort/switch.py
+++ custom_components/broan_chromacomfort/switch.py
@@ -1,26 +1,36 @@
-"""Switch platform for ChromaComfort."""
+"""Switch platform for ChromaComfort integration."""
 
 from homeassistant.components.switch import SwitchEntity
-from .const import DOMAIN
-from .bluetooth import rgb_on_cmd, rgb_off_cmd
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    ble_client = hass.data[DOMAIN]["ble_client"]
-    async_add_entities([ChromaComfortSwitch(ble_client)])
+from .ble import ChromaComfortBLE
 
 class ChromaComfortSwitch(SwitchEntity):
-    def __init__(self, ble_client):
-        self._ble = ble_client
+    """Representation of a ChromaComfort Switch."""
+
+    def __init__(self, ble: ChromaComfortBLE, name="Wall RGB"):
+        self._ble = ble
         self._is_on = False
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     @property
     def is_on(self):
         return self._is_on
 
     async def async_turn_on(self, **kwargs):
-        await self._ble.send_command(rgb_on_cmd())
         self._is_on = True
+        CMD = bytes([58, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0])
+        await self._ble.send_command(CMD)
+        self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        await self._ble.send_command(rgb_off_cmd())
         self._is_on = False
+        CMD = bytes([58, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0])
+        await self._ble.send_command(CMD)
+        self.async_write_ha_state()
+
+    async def async_refresh_state(self):
+        """Fetch latest state from BLE device."""
+        await self._ble.refresh_state()
