1f6f27a239a11d7ceacbca0cdcc1f5f59490a1a8
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 18:33:03 -0700

    Update fan.py

--- custom_components/broan_chromacomfort/fan.py
+++ custom_components/broan_chromacomfort/fan.py
@@ -1,35 +1,49 @@
-"""ChromaComfort fan platform."""
+"""Fan platform for ChromaComfort."""
 
-from homeassistant.components.fan import FanEntity, FanEntityFeature
+from homeassistant.components.fan import FanEntity, SUPPORT_SET_SPEED
+from homeassistant.helpers.entity import Entity
+
+from .const import DOMAIN
+
+SPEEDS = ["low", "medium", "high"]
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up fan from a config entry."""
+    ble_client = hass.data[DOMAIN]["ble_client"]
+    async_add_entities([ChromaComfortFan(ble_client)], True)
 
 class ChromaComfortFan(FanEntity):
-    """ChromaComfort Fan entity."""
+    """Representation of the ChromaComfort fan."""
 
     def __init__(self, ble_client):
         self._ble = ble_client
         self._is_on = False
-        self._speed = None
+        self._speed = "medium"
 
     @property
-    def supported_features(self):
-        return FanEntityFeature.SET_SPEED
+    def name(self):
+        return "ChromaComfort Fan"
 
     @property
     def is_on(self):
         return self._is_on
 
+    @property
+    def speed(self):
+        return self._speed
+
+    @property
+    def speed_list(self):
+        return SPEEDS
+
     async def async_turn_on(self, speed=None, **kwargs):
-        await self._ble.send_cmd(cmd_type=1)  # 1 = fan on
         self._is_on = True
+        if speed:
+            self._speed = speed
+        await self._ble.set_fan(self._is_on, self._speed)
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        await self._ble.send_cmd(cmd_type=2)  # 2 = fan off
         self._is_on = False
-        self.async_write_ha_state()
-
-    async def async_set_speed(self, speed: str):
-        speed_map = {"low": 10, "medium": 30, "high": 60}
-        await self._ble.send_cmd(cmd_type=0, dimmer=speed_map.get(speed, 30))
-        self._speed = speed
+        await self._ble.set_fan(self._is_on)
         self.async_write_ha_state()
