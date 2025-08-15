bc79090942357a0fbc809caa55b347e24d07b977
Efiller91 <59405194+Efiller91@users.noreply.github.com>
2025-08-14 18:53:04 -0700

    Update fan.py

--- custom_components/broan_chromacomfort/fan.py
+++ custom_components/broan_chromacomfort/fan.py
@@ -1,35 +1,37 @@
 """Fan platform for ChromaComfort."""
 
-from homeassistant.components.fan import FanEntity, SUPPORT_ON_OFF
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.components.fan import FanEntity
 from .const import DOMAIN
+from .bluetooth import fan_on_cmd, fan_off_cmd
+
+SPEEDS = ["low", "medium", "high"]
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    ble_client = hass.data[DOMAIN]["ble_client"]
+    async_add_entities([ChromaComfortFan(ble_client)])
 
 class ChromaComfortFan(FanEntity):
-    """Representation of the ChromaComfort fan."""
-
-    def __init__(self, hass, ble_client):
+    def __init__(self, ble_client):
         self._ble = ble_client
         self._is_on = False
-        self._name = "ChromaComfort Fan"
-
-    @property
-    def name(self):
-        return self._name
+        self._speed = None
 
     @property
     def is_on(self):
         return self._is_on
 
-    @property
-    def supported_features(self):
-        return SUPPORT_ON_OFF
-
-    async def async_turn_on(self, **kwargs):
-        await self._ble.turn_fan(True)
+    async def async_turn_on(self, speed=None, **kwargs):
+        await self._ble.send_command(fan_on_cmd())
         self._is_on = True
-        self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        await self._ble.turn_fan(False)
+        await self._ble.send_command(fan_off_cmd())
         self._is_on = False
-        self.async_write_ha_state()
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @property
+    def speed_count(self):
+        return len(SPEEDS)
